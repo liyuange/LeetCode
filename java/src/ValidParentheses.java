231b629b7a28f12a08197aea14dd9a31c8216294
liyuange <329240198@qq.com>
2020-06-08 14:49:05 +0800

    20.Valid Parentheses

--- java/src/ValidParentheses.java
+++ java/src/ValidParentheses.java
@@ -1,2 +1,25 @@
+import java.util.HashMap;
+import java.util.Stack;
+
 public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                char top = stack.empty() ? '!' : stack.pop();
+                if (top != map.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
 }
